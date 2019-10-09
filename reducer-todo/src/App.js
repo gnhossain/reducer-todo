5dd3a3d63dac6b366214bfcfc7a0dd0d4048fc8d
G H <gh@Gs-MacBook-Pro.local>
2019-10-08 17:50:44 -0700

    mvp

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import TodoForm from './components/TodoForm'
+
+function App() {
+
+    return (
+      <div className='App'>
+
+        <header>Reducer Todo</header>
+        <TodoForm />
+      </div>
+    )
+}
+
+export default App;
